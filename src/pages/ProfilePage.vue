f16fe381b0bb538d0feec3e04c98731a48485419
Annika <nikzneko@gmail.com>
2022-06-19 11:33:45 -0600

    can now view posts on profile pages

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="about text-center">
+  <img :src="profile.coverImg" class="profile-banner">
+  <div class="profile-name">
+    <img class="profile-picture" :src="profile.picture" alt="" />
     <h1>{{ profile.name }}</h1>
-    <img class="rounded" :src="profile.picture" alt="" />
+  </div>
+  <div class="post-container">
+    <Post v-for="p in posts" :key="p._id" :post="p" />
   </div>
 </template>
 
@@ -12,11 +16,13 @@ import { logger } from '../utils/Logger'
 import { useRoute } from "vue-router";
 import Pop from '../utils/Pop'
 import { profilesService } from "../services/ProfilesService"
+import { postsService } from '../services/PostsService';
 export default {
   name: 'Profile',
   setup() {
     const route = useRoute()
     onMounted(async () => {
+      // Load profile
       try {
         // call profilesService
         await profilesService.get(route.params.id)   // dummy id for now
@@ -25,16 +31,69 @@ export default {
         logger.error(error)
         Pop.toast(error.message, 'error')
       }
+
+      // Load user's posts
+      try {
+        AppState.posts = []
+        postsService.getByCreator(route.params.id)
+      } catch (error) {
+        logger.error(error)
+        Pop.toast(error.message, 'error')
+      }
     })
     return {
-      profile: computed(() => AppState.profile)
+      profile: computed(() => AppState.profile),
+      posts: computed(() => AppState.posts)
     }
   }
 }
 </script>
 
+
+  <!-- setup() {
+    onMounted(async () => {
+      try {
+        // add call to service here
+        postsService.getAll()
+      } catch (error) {
+        Pop.toast(error.message, "error")
+        logger.error(error)
+      }
+    })
+    return {
+      posts: computed(() => AppState.posts)
+    }
+  } -->
+
+
 <style scoped>
-img {
-  max-width: 100px;
+.profile-picture {
+  width: 96px;
+  height: 96px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin: 0px;
+  padding: 0px;
+}
+
+.profile-banner {
+  width: 100%;
+  height:  30vh;
+  object-fit: cover;
+  height: 320px;
+}
+
+.profile-name {
+  position: relative;
+  top: -48px;
+  display: flex;
+}
+
+.profile-name h1 {
+  align-self: flex-end;
+}
+
+.post-container {
+  width: 75%;
 }
 </style>
--- src/services/PostsService.js
+++ src/services/PostsService.js
@@ -10,6 +10,13 @@ import { api } from "./AxiosService"
 class PostsService {
   async getAll() {
     const res = await api.get('api/posts')
+    // logger.log(res.data.posts)
+    AppState.posts = res.data.posts
+  }
+
+  // api/profiles/:id/posts
+  async getByCreator(creatorId) {
+    const res = await api.get('api/profiles/' + creatorId + '/posts')
     logger.log(res.data.posts)
     AppState.posts = res.data.posts
   }
